169d6683b84f28a1d7ae88c4d170a12ebd95d19a
Mert oğur <mert.ogur10@gmail.com>
2022-01-26 15:32:31 +0300

    gönderi

---
+++ main.go
@@ -0,0 +1,19 @@
+//command ls -la
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func main() {
+
+	out, err := exec.Command("ls", "-l").Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(out))
+}
